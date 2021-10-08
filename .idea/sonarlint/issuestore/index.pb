1b152ee0d1495d6c0f2f3788db60fe9216e4b5f7
HamnYce <hamadboa@gmail.com>
2021-10-09 00:51:14 +0300

    -

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-9
-	winner.rb,2/a/2aa83d4b34219189db34bcca3f1c57ad1ea3ea5b
--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@
 # board. e.g. placing/updating the board
 class Board
   def initialize
+    @turns_left = 9
     @board = %w[123 --- --- ---]
     puts 'this is the board'
     print_board
@@ -11,7 +12,7 @@ class Board
 
   def place_tic_tac(pos, tic_tac)
     # pos[0] == row, pos[1] == col
-    return false unless index_valid_size?(pos) && index_valid_pos?(pos)
+    return false unless index_valid_size?(pos) && index_empty_pos?(pos)
 
     @board[pos[0]][pos[1] - 1] = tic_tac
     true
@@ -29,6 +30,10 @@ class Board
     @board[1, 3]
   end
 
+  def to_s
+    @board[1, 3]
+  end
+
   private
 
   def index_valid_size?(index)
@@ -40,7 +45,7 @@ class Board
     false
   end
 
-  def index_valid_pos?(index)
+  def index_empty_pos?(index)
     @board[index[0]][index[1] - 1] == '-'
   end
 end
