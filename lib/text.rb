d7383ef1accfa076cf3a2f940ccaf50d02ddf0b0
HamnYce <hamadboa@gmail.com>
2022-03-03 10:10:46 +0300

    rename menu to text, add end statement

---
+++ lib/text.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Module contains data for the visual portions of the game.
+module Text
+  TITLE =
+    <<-TITLE
+        ╔════╗─╔════╗───╔════╗
+        ║╔╗╔╗║─║╔╗╔╗║───║╔╗╔╗║
+        ╚╝║║╠╬═╩╣║║╠╩═╦═╩╣║║╠╩═╦══╗
+        ──║║╠╣╔═╝║║║╔╗║╔═╝║║║╔╗║║═╣
+        ──║║║║╚═╗║║║╔╗║╚═╗║║║╚╝║║═╣
+        ──╚╝╚╩══╝╚╝╚╝╚╩══╝╚╝╚══╩══╝
+    TITLE
+
+  def print_title
+    print TITLE
+  end
+
+  def print_intro_dialogue
+    # puts 'decided?'
+    puts 'okay, so here\'s what the board currently looks like'
+    # puts "your job, #{player_one}, is to make a line of Xs"
+    # puts 'sounds easy right?'
+    # puts 'But your not alone!'
+    # puts "#{player_two} will be blocking and trying to win as well..."
+    # puts 'but with Os!'
+    puts 'BEGIN!'
+  end
+
+  def print_error_text
+    puts "\e[31minvalid input, please try again ^_^\e[0m"
+    sleep 0.25
+    puts '----'
+  end
+end
