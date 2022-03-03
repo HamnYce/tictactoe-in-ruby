864822be941cc78eea3835af0598987d482c6356
HamnYce <hamadboa@gmail.com>
2022-03-03 10:12:45 +0300

    rework variable names, discover instance variables can be accessed from modules!

--- lib/winner.rb
+++ lib/winner.rb
@@ -6,45 +6,39 @@ module Winner
   # (0,0)(0,1)(0,2)
   # (1,0)(1,1)(1,2)
   # (2,0)(2,1)(2,2)
-  def winner?(board)
-    check_horizontal(board) or check_vertical(board) or check_diagonal(board)
+  def game_over?
+    check_horizontal || check_vertical || check_diagonal
   end
 
-  def check_horizontal(board)
-    x = same?(board, [0, 0], [0, 1], [0, 2])
-    y = same?(board, [1, 0], [1, 1], [1, 2])
-    z = same?(board, [2, 0], [2, 1], [2, 2])
-    x or y or z
+  def check_horizontal
+    first_row = same?([0, 0], [0, 1], [0, 2])
+    second_row = same?([1, 0], [1, 1], [1, 2])
+    third_row = same?([2, 0], [2, 1], [2, 2])
+
+    first_row || second_row || third_row
   end
 
-  def check_vertical(board)
-    x = same?(board, [0, 0], [1, 0], [2, 0])
-    y = same?(board, [0, 1], [1, 1], [2, 1])
-    z = same?(board, [0, 2], [1, 2], [2, 2])
-    x or y or z
+  def check_vertical
+    first_column = same?([0, 0], [1, 0], [2, 0])
+    second_column = same?([0, 1], [1, 1], [2, 1])
+    third_column = same?([0, 2], [1, 2], [2, 2])
+
+    first_column || second_column || third_column
   end
 
-  def check_diagonal(board)
-    x = same?(board, [0, 0], [1, 1], [2, 2])
-    y = same?(board, [2, 0], [1, 1], [0, 2])
-    x or y
+  def check_diagonal
+    leading_diag = same?([0, 0], [1, 1], [2, 2])
+    other_diag = same?([2, 0], [1, 1], [0, 2])
+    leading_diag || other_diag
   end
 
-  private
-
-  # compares position on the board using 2D vector coordinates.
-  # pos1[0] and pos1[1] => (x,y)
-  # if all 3 coordinates are the same AND not '-' then winner is present.
-  def same?(board, pos1, pos2, pos3)
-    first = board[pos1[0]][pos1[1]]
-    second = board[pos2[0]][pos2[1]]
-    third = board[pos3[0]][pos3[1]]
-
-    return false if first == '-' && second == '-' && third == '-'
+  def same?(pos1, pos2, pos3)
+    first = @board.to_s[pos1[0]][pos1[1]]
+    second = @board.to_s[pos2[0]][pos2[1]]
+    third = @board.to_s[pos3[0]][pos3[1]]
 
     xy = first == second
-    xz = first == third
-    xy && xz
+    xz = second == third
+    xy && xz && first != '-'
   end
 end
-
