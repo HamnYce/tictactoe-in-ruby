b7d66438650c9c109a9790bb504f4663271cdef4
HamnYce <hamadboa@gmail.com>
2022-03-03 10:09:19 +0300

    refactor methods

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,30 +1,33 @@
 # frozen_string_literal:true
 
-require_relative 'menu'
+require_relative 'text'
 require_relative 'winner'
 require_relative 'board'
 
 # Driver class acts as glue for all the different classes.
 class Driver
-
-  include Menu
+  include Text
   include Winner
 
   def initialize
     print_title
+    @turn_count = 0
+    init_player_names
+    print_intro_dialogue
     start
   end
 
   # init new board, turn counter, & player names, + starts gameloop
   def start
-    @turn_count = 0
-    init_player_names
-    print_intro_dialogue
     @board = Board.new
     game_loop
   end
 
-  private
+  def game_loop
+    turn until game_over? || @turn_count == 9
+
+    end_game_statement
+  end
 
   # use turn count to find current sign
   # repeats until correct position is received
@@ -39,6 +42,12 @@ class Driver
     @turn_count += 1
   end
 
+  def current_play
+    @sign == 'X' ? @player_one : @player_two
+  end
+
+  private
+
   def init_player_names
     puts 'decide on player 1 with a game of rock, paper, scissors!'
     puts 'player 1 what\'s your name?'
@@ -47,19 +56,12 @@ class Driver
     @player_two = gets.chomp
   end
 
-  def game_loop
-    turn until winner?(@board.to_s) || @turn_count == 9
-    unless winner?(@board.to_s)
+  def end_game_statement
+    if game_over?
+      puts "#{current_play} is the winner!!!!"
+    else
       puts 'Game ended in a draw ^^'
-      return
     end
-    puts "#{@sign == 'X' ? @player_one : @player_two} is the winner!!!!"
-  end
-
-  def print_error_text
-    puts "\e[31minvalid input, please try again ^_^\e[0m"
-    sleep 0.25
-    puts '----'
   end
 
   def get_position
